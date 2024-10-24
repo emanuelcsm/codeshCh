c626c535854d0753baefc3699c28fbd66219ec65
emanuelcsm <emanuelcsm@yahoo.com.br>
2024-10-24 17:04:51 -0300

    template api modified

--- src/Pokemons/Controllers/WeatherForecastController.cs
+++ src/Pokemons/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace Pokemons.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
+        [HttpGet] 
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
